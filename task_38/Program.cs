798033f2513b0cf4f2afe9e0e6709b7a39b8a65e
Aleksandr Guskov <bufanda@bk.ru>
2023-01-23 19:07:30 +0300

    task38

---
+++ task_38/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.7; 7.2; 2.1; 7.8] -> 5.7
+
+double[] FillArrayWithRandom(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble() + new Random().Next(0,40); 
+    }
+    return array;
+}
+double[] array = new double [5];
+array = FillArrayWithRandom(array);
+Console.Write("Ваш случайный массив: ");
+Console.WriteLine(string.Join(",  " , array));
+
+double max = 0;
+double min = array[0];
+
+for(int i = 0; i < array.Length; i++)
+{
+      if(array[i] > max)
+    {
+        max=array[i];
+    }
+}
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] < min)
+    {
+        min=array[i];
+    }
+}
+
+double sum = max-min;
+
+System.Console.WriteLine($"Разница между max и min = {sum}");
